b29319605f4ef063011e95651c892d21fa15d02e
cochoafge <cochoa@fiscaliaveracruz.gob.mx>
2018-04-04 15:58:13 -0500

    Header base

---
+++ inc/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Validador de Concesiones Vehiculares</title>
+		<link rel="stylesheet" href="../../css/bootstrap.min.css">
+	</head>
+	<body>
+		<button type="button" class="btn btn-primary">Primary</button>
+	</body>
+</html>
